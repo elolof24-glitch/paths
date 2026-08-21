d4ba2b02101db65af348fb12a73d61d25e399ee3
elolof24-glitch <elolof24@gmail.com>
2026-08-21 16:27:49 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
+import 'dotenv/config';
 import { startDiscord } from './src/discord.js';
 
-await startDiscord();
+console.log('[startup] index.js loaded');
+
+if (!process.env.DISCORD_TOKEN) {
+  throw new Error('DISCORD_TOKEN is missing in Railway Variables');
+}
+
+try {
+  await startDiscord();
+  console.log('[startup] Discord client started');
+} catch (error) {
+  console.error('[startup] Discord startup failed:', error);
+  process.exit(1);
+}
